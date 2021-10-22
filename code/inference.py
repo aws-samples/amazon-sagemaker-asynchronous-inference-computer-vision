e65bb58f93d6015c9066fa0a69d69f0e83197c3b
Hasan <hpoonawa@amazon.com>
2021-10-22 17:48:35 +0100

    model-chaining

--- code/inference.py
+++ code/inference.py
@@ -6,24 +6,38 @@ import json
 import torch 
 import numpy as np
 import torchvision.transforms as transforms
+import torchvision.models as models
 from six import BytesIO
 import io
 from PIL import Image
 import cv2
 import tempfile
 
-
-
 logger = logging.getLogger(__name__)
-
+#A single chained model 
+class Model(torch.nn.Module):
+    def __init__(self,model,model2):
+        super(Model, self).__init__()
+        self.models = torch.nn.ModuleList()
+        self.models.append(model)
+        self.models.append(model2)
+    def forward(self, input):
+        results = {}
+        for i,x in enumerate(self.models):
+            results[i] = x(input)
+        return results
 
 def model_fn(model_dir):
     device = get_device()
     print('device is')
     print(device)
-    model = torch.load(model_dir + '/model.pth', map_location=torch.device(device))
-    print(type(model))
-    return model
+    model = models.detection.maskrcnn_resnet50_fpn(pretrained=False)
+    model2 = models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
+    chained_model = Model(model,model2)
+    chained_model.load_state_dict(torch.load(model_dir + '/model.pth', map_location=torch.device(device)))
+    chained_model.eval()
+    
+    return [chained_model,chained_model] # can do model chaining here as well
 
 
 def input_fn(request_body, request_content_type):
@@ -43,11 +57,11 @@ def input_fn(request_body, request_content_type):
 
     return image_tensors
 
-def predict_fn(data, model):
+def predict_fn(data, models):
     print('in custom predict function')
     with torch.no_grad():
         device = get_device()
-        model = model.to(device)
+        model = models[0].to(device)
         input_data = data.to(device)
         model.eval()
         output = model(input_data)
@@ -55,11 +69,16 @@ def predict_fn(data, model):
     return output
 
     
-def output_fn(output_batch, accept='application/json'):
+def output_fn(output_batches, accept='application/json'):
     res = []
+    output_batch1 = output_batches[0] # model 1
+    output_batch2 = output_batches[1] # model 2
     print('output list length')
-    print(len(output_batch))
-    for output in output_batch:
+    print(len(output_batch1))
+    for output in output_batch1:
+         res.append({'boxes':output['boxes'].detach().cpu().numpy().tolist(),'labels':output['labels'].detach().cpu().numpy().tolist(),'scores':output['scores'].detach().cpu().numpy().tolist()})
+            
+    for output in output_batch2:
          res.append({'boxes':output['boxes'].detach().cpu().numpy().tolist(),'labels':output['labels'].detach().cpu().numpy().tolist(),'scores':output['scores'].detach().cpu().numpy().tolist()})
     
     return json.dumps(res)
